cfad973e129809a90f0527e0e98c92177a1e518a
Evellyn Silva <evellynmaria593@gmail.com>
2024-11-11 08:37:06 -0300

    Iniciando Estudos

---
+++ script.js
@@ -0,0 +1,10 @@
+const valor1 = 10;
+const valor2 = 2;
+
+console.log('soma', valor1 + valor2) //printa o resultado//
+
+console.log('subtração', valor1 - valor2) 
+
+console.log('Multiplicação', valor1 * valor2) 
+
+console.log('Divisão', valor1 / valor2) 
